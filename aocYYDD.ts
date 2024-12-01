9f8c1cd83521eaf5d4322bbdf6b9495d9aba775d
Roger Hoem-Martinsen <roger.h.martinsen@gmail.com>
2024-12-01 14:54:57 +0100

    day1

---
+++ aocYYDD.ts
@@ -0,0 +1,14 @@
+import { NotImplemented, run } from 'aoc-copilot';
+
+async function solve(
+    inputs: string[], // Contents of the example or actual inputs
+    part: number,     // Indicates whether the solver is being called for part 1 or 2 of the puzzle
+    test: boolean,    // Indicates whether the solver is being run for an example or actual input
+    additionalInfo?: { [key: string]: string } // Additional info for some puzzles with multiple examples
+): Promise<number | string> {
+    let answer: number | string = 0;
+    throw new NotImplemented('Not implemented'); // <-- Replace with your solution
+    return answer;
+}
+
+run(__filename, solve);
