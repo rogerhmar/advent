12edcf58896ca5399446d63a07323f6ca6fb58a7
Roger Hoem-Martinsen <roger.h.martinsen@gmail.com>
2024-12-10 13:28:04 +0100

    day9: cleanup

--- 2024/solutions/aoc2410.ts
+++ 2024/solutions/aoc2410.ts
@@ -2,46 +2,39 @@ import {run} from "aoc-copilot";
 import {getDiff, isOutside, Position} from "../../utils/position";
 import {InputAsNumbers} from "../../utils/div";
 
-function getCandidates(p: Position, map: number[][]) {
+function getCandidates(from: Position, map: number[][]) {
     const candidates: Position[] = []
 
-    const down = {y:p.y + 1, x:p.x}
-    const up = {y:p.y - 1, x:p.x}
-    const left = {y:p.y, x:p.x - 1}
-    const right = {y:p.y, x:p.x + 1}
+    const down = {y:from.y + 1, x:from.x}
+    const up = {y:from.y - 1, x:from.x}
+    const left = {y:from.y, x:from.x - 1}
+    const right = {y:from.y, x:from.x + 1}
 
-    for (const dir of [down, up, left, right]) {
-        const inside = !isOutside(dir, map)
-        if (inside) {
-            const diff = getDiff(p, dir, map)
-            if(diff === 1) {
-                candidates.push(dir);
+    for (const to of [down, up, left, right]) {
+        if (!isOutside(to, map)) {
+            if(getDiff(from, to, map) === 1) {
+                candidates.push(to);
             }
         }
     }
     return candidates
 }
 
-export function getNumberOfPaths(trailhead: Position, map: number[][], distict: boolean) {
+export function getNumberOfPaths(trailhead: Position, map: number[][], distinct: boolean) {
     let heads = [trailhead]
     let allDone = []
-    const max = 9
-    const isDone = (pos: Position) => map[pos.y][pos.x] === max
+    const MAX = 9
+    const isDone = (pos: Position) => map[pos.y][pos.x] === MAX
 
     while (true) {
         const path = heads.shift()
         if (path != undefined) {
             const candidates = getCandidates(path, map)
-            const candidatesDone = candidates.filter(c => isDone(c))
-            if (candidatesDone.length > 0) {
-                allDone.push(...candidatesDone)
-            }
-            const candidatesNotDone = candidates.filter(c => !isDone(c))
-            if (candidatesNotDone.length > 0)
-                heads.push(...candidatesNotDone)
+            allDone.push(...candidates.filter(c => isDone(c)))
+            heads.push(...candidates.filter(c => !isDone(c)))
         } else {
             let str = allDone.map(d => JSON.stringify(d))
-            if(distict)
+            if(distinct)
                 return new Set(str).size
             else
                 return str.length
@@ -69,21 +62,7 @@ export function solveDay10(inputs: string[], distict: boolean) {
 async function solve(
     inputs: string[], // Contents of the example or actual inputs
     part: number,     // Indicates whether the solver is being called for part 1 or 2 of the puzzle
-    test: boolean,
 ): Promise<number | string> {
-
-    if (test) {
-        inputs = [
-            "89010123",
-            "78121874",
-            "87430965",
-            "96549874",
-            "45678903",
-            "32019012",
-            "01329801",
-            "10456732"
-        ]
-    }
     return solveDay10(inputs, part == 1)
 }
 
